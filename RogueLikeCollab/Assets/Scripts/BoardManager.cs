54327e41f3bad685cd8510c09ee2801dbd7d6f68
R3ST4RT1NG <dil.tuff@gmail.com>
2018-12-29 22:48:47 -0700

    -Added basic dungeon generation based on the following tutorial https://unity3d.com/learn/tutorials/projects/2d-roguelike-tutorial/writing-game-manager?playlist=17150 -Added wall and Floor Sprites based on pixel dungeon assets (tiles need to be fixed as they have wierd sizing issues) -Added a boardManager and gameManager that are used to generate and load the dungeon on startup

---
+++ RogueLikeCollab/Assets/Scripts/BoardManager.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class BoardManager : MonoBehaviour {
+
+    [Serializable]
+    public class Count
+    {
+        public int minimum;
+        public int maximum;
+
+        public Count(int min, int max)
+        {
+            minimum = min;
+            maximum = max;
+        }
+    }
+
+    public int columns = 8;
+    public int rows = 8;
+    public Count wallCount = new Count(5, 9);
+    public GameObject wall;
+    public GameObject floor;
+
+    private Transform boardHolder;
+    private List<Vector3> gridPositions = new List<Vector3>();
+
+    private void InitializeList()
+    {
+        gridPositions.Clear();
+
+        for(int x = 1; x < columns - 1; x++)
+        {
+            for(int y = 1; y < rows - 1; y++)
+            {
+                gridPositions.Add(new Vector3(x, y, 0f));
+            }
+        }
+    }
+
+    private void BoardSetup()
+    {
+        boardHolder = new GameObject("Board").transform;
+        
+        for(int x = -1; x < columns + 1; x++)
+        {
+            for(int y = -1; y < rows + 1; y++)
+            {
+                GameObject toInstantiate = floor;
+                if (x == -1 || x == columns || y == -1 || y == rows)
+                {
+                    toInstantiate = wall;
+                }
+
+                GameObject instance = Instantiate(toInstantiate, new Vector3(x, y, 0f), Quaternion.identity) as GameObject;
+
+                instance.transform.SetParent(boardHolder);
+            }
+        }
+    }
+
+    private Vector3 RandomPosition()
+    {
+        int randomIndex = Random.Range(0, gridPositions.Count);
+        Vector3 randomPosition = gridPositions[randomIndex];
+        gridPositions.RemoveAt(randomIndex);
+
+        return randomPosition;
+    }
+
+    private void LayoutObjectAtRandom(GameObject tile, int minimum, int maximum)
+    {
+        int objectCount = Random.Range(minimum, maximum + 1);
+
+        for (int i = 0; i < objectCount; i++)
+        {
+            Vector3 randomPosition = RandomPosition();
+            Instantiate(tile, randomPosition, Quaternion.identity);
+        }
+    }
+
+    public void SetupScene(int level)
+    {
+        BoardSetup();
+        InitializeList();
+        LayoutObjectAtRandom(wall, wallCount.minimum, wallCount.maximum);
+    }
+}
